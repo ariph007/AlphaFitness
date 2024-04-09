5c843c85ba1fec669acfcf8c2b8cc20e93762a20
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2024-04-09 22:12:39 +0700

    seperate styles

--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { AntDesign } from "@expo/vector-icons";
 
@@ -10,21 +10,34 @@ type Iitem = {
 
 const FoodListItem = (item:Iitem) => {
     return (
-        <View style={{
-          backgroundColor: 'gainsboro',
-          padding: 10,
-          borderRadius: 5,
-          flexDirection: "row",
-          justifyContent: 'space-between',
-          alignItems: 'center'
-        }}> 
-          <View style={{flex: 1, gap:5}}>
-            <Text style={{fontWeight: 'bold', fontSize:16}}>{item.label}</Text>
-            <Text style={{color: 'dimgray'}}>{item.cal} cal, {item.brand}</Text>
+        <View style={styles.container}> 
+          <View style={styles.listWrapper}>
+            <Text style={styles.title}>{item.label}</Text>
+            <Text style={styles.bodyItem}>{item.cal} cal, {item.brand}</Text>
           </View>
           <AntDesign name="pluscircleo" size={24} color="royalblue" />
         </View>
       )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'gainsboro',
+        padding: 10,
+        borderRadius: 5,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    listWrapper: {
+        flex: 1, gap:5
+    },
+    title:{
+        fontWeight: 'bold', fontSize:16
+    },
+    bodyItem:{
+        color: 'dimgray'
+    }
+})
 
 export default FoodListItem
