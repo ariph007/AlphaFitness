46f7304fa9a2eb8edb9da374bdf26f953d4a59bd
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2024-04-10 00:18:30 +0700

    update foodlist component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import FoodListItem from "../components/FoodListItem";
+import { useState } from "react";
+
+const foodItems = [
+  { label: "Pizza", cal: 75, brand: "Dominos" },
+  { label: "Burger", cal: 122, brand: "Burger King" },
+  { label: "Chicken", cal: 253, brand: "KFC" },
+];
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
+  const performSearch = () => {
+    console.warn("Search :" + search);
+    setSearch("");
+  };
+
   return (
     <View style={styles.container}>
-      <FoodListItem label="Pizza" cal={75} brand="Dominos" />
-      <FoodListItem label="Burger" cal={212} brand="Burger King" />
-  </View>
+      <TextInput
+        value={search}
+        onChangeText={setSearch}
+        placeholder="Search..."
+        style={styles.search}
+      />
+      {search && (
+        <Pressable onPress={performSearch} style={styles.btnSearch}>
+          <Text style={styles.btnTxtSearch}>Search</Text>
+        </Pressable>
+      )}
+      <FlatList
+        data={foodItems}
+        renderItem={({ item }) => (
+          <FoodListItem label={item.label} cal={item.cal} brand={item.brand} />
+        )}
+        contentContainerStyle={{ gap: 5 }}
+      />
+    </View>
   );
 }
 
@@ -14,8 +52,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
     padding: 10,
-    gap: 5
+    gap: 10,
   },
+  search: {
+    backgroundColor: "gainsboro",
+    padding: 10,
+    borderRadius: 20,
+  },
+  btnTxtSearch: {
+    color: "royalblue",
+    fontSize: 16,
+    fontWeight: "600"
+  },
+  btnSearch:{
+    flexDirection: "row",
+    justifyContent: "center"
+  }
 });
--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -8,12 +8,12 @@ type Iitem = {
     brand: string
   }
 
-const FoodListItem = (item:Iitem) => {
+const FoodListItem = ({label, cal, brand}: Iitem) => {
     return (
         <View style={styles.container}> 
           <View style={styles.listWrapper}>
-            <Text style={styles.title}>{item.label}</Text>
-            <Text style={styles.bodyItem}>{item.cal} cal, {item.brand}</Text>
+            <Text style={styles.title}>{label}</Text>
+            <Text style={styles.bodyItem}>{cal} cal, {brand}</Text>
           </View>
           <AntDesign name="pluscircleo" size={24} color="royalblue" />
         </View>
@@ -22,21 +22,21 @@ const FoodListItem = (item:Iitem) => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'gainsboro',
+        backgroundColor: "#f6f6f9",
         padding: 10,
         borderRadius: 5,
         flexDirection: "row",
-        justifyContent: 'space-between',
-        alignItems: 'center'
+        justifyContent: "space-between",
+        alignItems: "center"
     },
     listWrapper: {
         flex: 1, gap:5
     },
     title:{
-        fontWeight: 'bold', fontSize:16
+        fontWeight: "bold", fontSize:16
     },
     bodyItem:{
-        color: 'dimgray'
+        color: "dimgray"
     }
 })
 
